1a93511125baaaa6de8686f597b0ed432778343a
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2023-12-06 17:41:22 -0300

    [GS] Jina, Macros and Advanced Materializations courses completed

--- src/models/staging/stripe/stg_payments.sql
+++ src/models/staging/stripe/stg_payments.sql
@@ -15,10 +15,11 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
+    amount as payment_amount,
     status,
 
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
+    {{ cents_to_dollars('payment_amount', 4) }} as amount,
     created as created_at
 
 from  {{ source('stripe', 'payment') }}
